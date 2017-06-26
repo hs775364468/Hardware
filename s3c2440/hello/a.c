abcfb69220e88db60319b1b503077e31d29416db
heshuai <13813327632@163.com>
2017-06-26 12:22:35 +0800

    1th version add hello.c

---
+++ s3c2440/hello/a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void a_func(void)
+{
+	printf("this is a_func!\n");
+
+}
---
+++ s3c2440/hello/hello.c
@@ -0,0 +1,8 @@
+extern void a_func(void);
+#include <stdio.h>
+int  main(void)
+{
+	printf("this is main func!\n");
+	a_func();
+	return 0;
+}
